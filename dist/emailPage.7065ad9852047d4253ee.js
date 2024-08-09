aaa80b2fdc8a61599165990a887e330fda7e8e65
krig6 <ej.ldsm@gmail.com>
2024-08-09 19:24:17 +0800

    Update build files

---
+++ dist/emailPage.7065ad9852047d4253ee.js
@@ -0,0 +1,18 @@
+(self["webpackChunkodin_restaurant"] = self["webpackChunkodin_restaurant"] || []).push([["emailPage"],{
+
+/***/ "./src/javascript/pages/emailPage.js":
+/*!*******************************************!*\
+  !*** ./src/javascript/pages/emailPage.js ***!
+  \*******************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/javascript/pages/emailPage.js"));
+/******/ }
+]);
---
+++ dist/homePage.75749cc48416fe2545b6.js
@@ -0,0 +1,18 @@
+(self["webpackChunkodin_restaurant"] = self["webpackChunkodin_restaurant"] || []).push([["homePage"],{
+
+/***/ "./src/javascript/pages/homePage.js":
+/*!******************************************!*\
+  !*** ./src/javascript/pages/homePage.js ***!
+  \******************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/javascript/pages/homePage.js"));
+/******/ }
+]);
