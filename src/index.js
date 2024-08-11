45b2bc97acc14e7985734bfcb21023ed2e28c128
krig6 <ej.ldsm@gmail.com>
2024-08-12 00:29:06 +0800

    feat: Add footer initialization to main script
    - Imported and initialized 'footerComponent' alongside 'mainNavigation'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/global.css'
 import { initializeMainNavigation } from "./javascript/components/mainNavigation";
+import { initializeFooterComponent } from './javascript/components/footerComponent';
 
 initializeMainNavigation();
+initializeFooterComponent();
