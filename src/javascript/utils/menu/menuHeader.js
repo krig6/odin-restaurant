fbbdc343fc304a2648ba33f598e44ed97785aee6
krig6 <ej.ldsm@gmail.com>
2024-08-26 13:26:49 +0800

    feat: Create menuHeader.js and add header setup functionality
    - Added 'setupMenuHeader' function to create and append the menu header
    - Implemented 'createCategoryButtons' to generate buttons for each category
    - Integrated 'initializeMenuEventHandlers' to handle category button interactions

---
+++ src/javascript/utils/menu/menuHeader.js
@@ -0,0 +1,32 @@
+import { initializeMenuEventHandlers } from "../menu/menuControls";
+import { createCustomElement } from "../domUtils/elementUtils";
+
+const CATEGORY_LINKS = ['cup', 'crust', 'churn', 'snack']
+
+export const setupMenuHeader = () => {
+    const contentElement = document.getElementById('main-content')
+    const header = createCustomElement('header', { classes: 'category-name' });
+    const nav = createCustomElement('nav', { classes: 'category-links' })
+    const ul = createCustomElement('ul', { classes: 'category' });
+
+
+    CATEGORY_LINKS.forEach(item => ul.appendChild(createCategoryButtons(item)));
+    nav.appendChild(ul);
+    header.appendChild(nav);
+    contentElement.appendChild(header);
+
+    initializeMenuEventHandlers();
+}
+
+
+const createCategoryButtons = (btnClass) => {
+    const li = createCustomElement('li');
+    const button = createCustomElement('button', {
+        classes: 'menu-btn',
+        dataset: { action: btnClass }
+    });
+    button.textContent = btnClass.charAt(0).toUpperCase() + btnClass.slice(1);
+    li.appendChild(button);
+
+    return li;
+}
