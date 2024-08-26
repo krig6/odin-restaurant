54d4b6f3192ef76f627d06725b87a4a690de15eb
krig6 <ej.ldsm@gmail.com>
2024-08-26 12:46:57 +0800

    feat: Add menuRenderer.js to handle menu item rendering
    - Created 'menuRenderer.js' for rendering menu items dynamically
    - Implemented 'renderMenuItems' function to generate the menu layout and display products
    - Added 'clearMenuWrapper' to remove existing menu content before rendering new items

---
+++ src/javascript/utils/menu/menuRenderer.js
@@ -0,0 +1,57 @@
+import { createCustomElement, createImageElement } from "../domUtils/elementUtils";
+
+
+export const renderMenuItems = (items) => {
+    clearMenuWrapper()
+
+    const content = document.getElementById('main-content');
+    const menuWrapper = createCustomElement('section', {
+        classes: 'category-products'
+    });
+
+
+    const grid = createCustomElement('section', {
+        classes: 'product-grid'
+    })
+
+    items.forEach(item => {
+        const article = createCustomElement('article');
+        const img = createImageElement(item.src, {
+            alt: item.alt
+        });
+
+        article.appendChild(img);
+
+        const productName = createCustomElement('span', {
+            text: item.alt,
+            classes: 'product-name'
+        })
+
+        article.appendChild(productName);
+
+        const desc = createCustomElement('p', {
+            text: item.description
+        });
+
+        article.appendChild(desc);
+
+        const price = createCustomElement('span', {
+            text: item.price
+        });
+        article.appendChild(price);
+
+        grid.appendChild(article);
+    });
+
+    menuWrapper.appendChild(grid)
+    content.appendChild(menuWrapper);
+}
+
+
+
+const clearMenuWrapper = () => {
+    const menu = document.querySelector('.category-products');
+    if (menu) {
+        menu.remove();
+    }
+}
