81e55bd909fbb48018d313bfd7ba17ffd0235de3
krig6 <ej.ldsm@gmail.com>
2024-09-07 00:05:08 +0800

    feat: Create aboutPageLayout.js to handle About page layout and sections
    - Added 'setupAboutMainImage' function to render the main About page image
    - Added 'populateAboutPageSections' function to dynamically load and display content sections
    - Loaded content data from 'aboutPageContent.json' and images dynamically

---
+++ src/javascript/utils/about/aboutPageLayout.js
@@ -0,0 +1,84 @@
+import { createCustomElement, createImageElement } from "../domUtils/elementUtils";
+import aboutSections from '../../data/aboutPageContent.json'
+import aboutMainImage from '../../../assets/images/about-page-images/about-image.jpg'
+import { fetchAllImages } from "../imageUtils/imageUtils";
+const fetchSegmentImages = fetchAllImages(require.context('../../../assets/images/about-page-images/', false, /\.(png|jpe?g|gif|svg)$/))
+
+export const setupAboutMainImage = () => {
+    const contentElement = document.getElementById('main-content');
+
+    if (!contentElement) {
+        console.log(`Element with id 'main-content' not found.`)
+        return;
+    }
+
+    const aboutMainContainer = createCustomElement('section', {
+        classes: 'about-main-container'
+    })
+
+    const imageWrapper = createCustomElement('section', {
+        classes: 'about-image-wrapper'
+    })
+
+    const image = createImageElement(aboutMainImage, {
+        classes: 'about-main-image'
+    })
+
+    imageWrapper.appendChild(image)
+    aboutMainContainer.appendChild(imageWrapper)
+    contentElement.appendChild(aboutMainContainer)
+}
+
+export const populateAboutPageSections = () => {
+    const contentElement = document.getElementById('main-content');
+
+    if (!contentElement) {
+        console.log(`Element with id 'main-content' not found.`)
+        return;
+    }
+
+    const aboutSectionWrapper = createCustomElement('section', {
+        classes: 'about-section-wrapper'
+    })
+
+    aboutSections.forEach(section => {
+        const { id, heading, image, content } = section
+
+        const aboutSection = createCustomElement('div', {
+            classes: `${id}-container about-section`
+        })
+
+        const aboutSectionText = createCustomElement('section', {
+            classes: `${id}-text about-section-text`,
+        })
+
+        const aboutSectionTitle = createCustomElement('h6', {
+            classes: 'about-section-title',
+            text: heading
+        })
+
+        const aboutSectionDescription = createCustomElement('div', {
+            classes: `${id}-description about-section-description`
+        })
+
+        const aboutSectionImage = createImageElement(fetchSegmentImages[image], {
+            classes: `${id}-image about-section-image`
+        })
+
+        if (content && content.length > 0) {
+            content.forEach(para => {
+                Object.keys(para).forEach(paragraphKey => {
+                    const paragraphElement = createCustomElement('p', {
+                        text: para[paragraphKey]
+                    })
+                    aboutSectionDescription.appendChild(paragraphElement)
+                })
+            })
+        }
+
+        aboutSectionText.append(aboutSectionTitle, aboutSectionDescription);
+        aboutSection.append(aboutSectionText, aboutSectionImage);
+        aboutSectionWrapper.appendChild(aboutSection)
+        contentElement.appendChild(aboutSectionWrapper)
+    })
+}
