5cbdf74a0aefa060881bdcffbe9e99384bbde728
krig6 <ej.ldsm@gmail.com>
2025-01-26 19:50:03 +0800

    fix: Update header sticky state for 'email' page
    - Modified 'updateHeaderStickyState' to make header sticky on both 'menu' and 'email' pages

--- src/javascript/utils/navigation/mainNavAppearance.js
+++ src/javascript/utils/navigation/mainNavAppearance.js
@@ -13,7 +13,7 @@ export const resetToDefaultNavColors = () => {
 }
 
 export const updateHeaderStickyState = (page) => {
-    const shouldStick = page === 'menu';
+    const shouldStick = page === 'menu' || page === 'email';
     setHeaderSticky(shouldStick);
 }
 
