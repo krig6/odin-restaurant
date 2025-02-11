0a124ed158418d099e30fe1dcb7787235382785f
krig6 <ej.ldsm@gmail.com>
2025-02-11 16:44:38 +0800

    chore: Install eslint and configure eslint script
    - Installed ESLint as a dev dependecy
    - Added "lint": "eslint src" to 'package.json' scripts
    - Created initial 'eslint.config.mjs' with recommended settings

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    settings: {
+      "import/resolver": {
+        webpack: {
+          config: "webpack.dev.js",
+        },
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
