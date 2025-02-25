b470220401e387bd804e76ccb5ace954e9a1a360
krig6 <ej.ldsm@gmail.com>
2025-02-25 21:35:43 +0800

    chore: Update ESLint config and package setup
    - Removed '@eslint/js' and its recommended config
    - Added custom ESLint rules
    - Enforced newline at the end of files with 'eol-last' rule
    - Updated 'package.json' to include 'type': 'module'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     languageOptions: { globals: globals.browser },
     settings: {
-      "import/resolver": {
+      'import/resolver': {
         webpack: {
-          config: "webpack.dev.js",
+          config: 'webpack.dev.js',
         },
       },
     },
+    rules: {
+      'object-curly-newline': [
+        'error',
+        {
+          'ImportDeclaration': { 'multiline': true, }
+        }
+      ],
+      'no-restricted-imports': ['error', 'underscore'],
+      'eqeqeq': 'error',
+      'no-console': 'off',
+      'consistent-return': 'error',
+      'no-unused-vars': 'off',
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'padding-line-between-statements': [
+        'error',
+        { 'blankLine': 'always', 'prev': 'import', 'next': '*' }
+      ],
+      'eol-last': ['error', 'always']
+    },
   },
-  pluginJs.configs.recommended,
 ];
