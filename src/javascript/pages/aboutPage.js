4a89e33c36da75facbcd0b6bb985775a7b264908
krig6 <ej.ldsm@gmail.com>
2025-03-10 21:31:07 +0800

    feat: Ensure light-colored navbar on About page
    - Refactored 'resetToDefaultNavColors' to keep the navbar light on the
    About page if '.change-color' is not present
    - Revised 'updateHeaderStickyState' to prevent the header from becoming
    sticky on the About page
    - Added 'toggleMainNavColors' inside 'setupAboutPageStructure' to ensure
    the default navbar color is light when navigating to the About page
    - Updated 'aboutComponent.css' to center the banner text on the image
    for better visual balance

--- src/javascript/pages/aboutPage.js
+++ src/javascript/pages/aboutPage.js
@@ -8,6 +8,7 @@ import {
 } from '../components/aboutComponent.js';
 
 import { deleteCarousel } from '../utils/carousel/carouselManager.js';
+import { toggleMainNavColors } from '../utils/navigation/mainNavAppearance.js';
 
 import { initializeNavEventHandlers } from '../utils/navigation/mainNavigationEvents.js';
 
@@ -18,6 +19,7 @@ import '../../styles/aboutComponent.css';
 export const setupAboutPageStructure = () => {
   deleteCarousel();
   clearMainContent();
+  toggleMainNavColors();
   createIntroBanner();
   createHeroImageSection();
   buildAboutSection();
--- src/javascript/utils/navigation/mainNavAppearance.js
+++ src/javascript/utils/navigation/mainNavAppearance.js
@@ -5,15 +5,25 @@ export const toggleMainNavColors = () => {
   });
 };
 
-export const resetToDefaultNavColors = () => {
+export const resetToDefaultNavColors = (page) => {
   const headerNavLinks = document.querySelectorAll('#site-header .nav-link');
-  headerNavLinks.forEach(link => {
-    link.classList.remove('change-color');
-  });
+  const hasChangeColor = Array.from(headerNavLinks).some(link => link.classList.contains('change-color')
+  );
+
+  if (page !== 'about') {
+    headerNavLinks.forEach(link => {
+      link.classList.remove('change-color');
+    });
+  } else {
+    if (!hasChangeColor) {
+      toggleMainNavColors();
+    }
+  }
+
 };
 
 export const updateHeaderStickyState = (page) => {
-  const shouldStick = page !== 'home';
+  const shouldStick = page !== 'home' && page !== 'about';
   setHeaderSticky(shouldStick);
 };
 
